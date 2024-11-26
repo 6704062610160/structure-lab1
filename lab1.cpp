d4e442adcadc71913b309c7e6413a8ff5c8d26ee
6704062610160 <s6704062610160@email.kmutnb.ac.th>
2024-11-26 15:02:35 +0700

    ข้อหนึ่งครับ

---
+++ lab1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int(main)(){
+	int value1;
+    int value2;
+    
+    if (value1 > value2){
+    	printf("value1 more than value2");
+	}
+    else{
+		printf("value1 not more than value2");
+	}
+    
+}
+
+
+
+    
+    
+
+
+                    
